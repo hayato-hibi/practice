94dafadb12479e32c58a33797fbe88b11481361c
hayato.hibi <hayatocats@gmail.com>
2020-11-23 22:45:22 +0900

    tapbo　borderの使い方

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(
+      child: TapboxC(
         active: _active,
         onChanged: _handleTapboxChanged,
       ),
@@ -25,33 +25,64 @@ class _ParentWidgetState extends State<ParentWidget> {
   }
 }
 
-//------------------------- TapboxB ----------------------------------
-
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
       : super(key: key);
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
+  _TapboxCstate createState() => _TapboxCstate();
+}
+
+class _TapboxCstate extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    widget.onChanged(!widget.active);
   }
 
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
       onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
       child: Container(
         child: Center(
           child: Text(
-            active ? 'Active' : 'Inactive',
+            widget.active ? 'Active' : 'Inactive',
             style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
         ),
       ),
     );
